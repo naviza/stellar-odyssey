fa23a49b2fe4a113104a38c45a786aaf08870890
Bianca Fu <0509biancafu@gmail.com>
2023-10-08 18:17:48 -0600

    fix bug

--- stellar-odyssey-frontend/src/components/Overview.js
+++ stellar-odyssey-frontend/src/components/Overview.js
@@ -3,7 +3,7 @@ import Spline from '@splinetool/react-spline';
 export default function Overview() {
   return (
     <div>
-        <Spline scene="https://prod.spline.design/6Tv0g2gt7U6yoovs/scene.splinecode" style={{marginLeft: '180px'}} />
+        <Spline scene="https://prod.spline.design/6Tv0g2gt7U6yoovs/scene.splinecode" style={{marginLeft: '180px', height: '504px'}} />
     </div>
   );
 }

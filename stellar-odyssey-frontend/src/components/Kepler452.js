d36d2955d690d6ef9af9ad8854f9f8c674e1dc9f
Bianca Fu <0509biancafu@gmail.com>
2023-10-08 21:44:52 -0600

    complete info details

--- stellar-odyssey-frontend/src/components/Kepler452.js
+++ stellar-odyssey-frontend/src/components/Kepler452.js
@@ -2,6 +2,6 @@ import Spline from '@splinetool/react-spline';
 
 export default function K452() {
   return (
-    <Spline scene="https://prod.spline.design/bJlySkVKCeri70RF/scene.splinecode" />
+    <Spline scene="https://prod.spline.design/cHn0kiDo28kgXPUT/scene.splinecode" />
   );
 }
--- stellar-odyssey-frontend/src/components/PlanetInfo.js
+++ stellar-odyssey-frontend/src/components/PlanetInfo.js
@@ -13,7 +13,7 @@ function PlanetInfo() {
           {/* Your slide content for the first item */}
           <Dome />
           <Carousel.Caption>
-            <h3 style={{fontSize: '35px'  }}>Dome</h3>
+            <h3 style={{fontSize: '35px'  }}>Dome Infrastructure</h3>
             <p style={{textAlign: 'justify'}}>Edmund's Planet features advanced habitat domes equipped with a self-contained information technology system for seamless internet access and connectivity within the domes. These dome infrastructures facilitate communication, data exchange, and coordination among the various facilities, including the main resort, vertical farming systems, meat/poultry cloning facilities, medical clinics, and security facilities.
               <br></br><br></br>During harsh conditions, residents and visitors find refuge in these domes, which will have an automated elevating feature, not only shielding the inhabitants from the planet’s challening terrain, but also offering a unique stargazing experience. This celestial retreat enhances their quality of life, safety, and well-being, providing an extraordinary connection with the cosmos within the domes.</p>
           </Carousel.Caption>
@@ -27,14 +27,36 @@ function PlanetInfo() {
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item style={{ height: '900px' }}>
+        <img src={require('./img/Slide_3.png')} style={{height: '680px', paddingTop: '180px'}} />
+
           {/* Your slide content for the third item */}
           <Carousel.Caption>
-            <h3 style={{fontSize: '35px'  }}>Third slide label</h3>
+                        <h3 style={{fontSize: '35px'  }}>Chemical, Temperature, Atmosphere</h3>
             <p style={{textAlign: 'justify'}}>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            Edmund's Planet boasts a chemical composition resembling Earth, with oxygen (O2), nitrogen (N2), carbon dioxide (CO2), water vapor (H2O), argon (Ar), and trace elements. However, during harsh conditions, such as the extended winters, the air composition changes, necessitating refuge within small advanced domes. These dome maintains the planet's atmosphere in a habitable and vacationable state, ensuring comfort and safety for residents and vacationers alike.
             </p>
           </Carousel.Caption>
-        </Carousel.Item>
+          </Carousel.Item>
+          <Carousel.Item style={{ height: '900px' }}>
+          <img src={require('./img/slide4.png')} style={{height: '700px', paddingTop: '180px'}} />
+
+          <Carousel.Caption>
+                        <h3 style={{fontSize: '35px'  }}>Surface</h3>
+            <p style={{textAlign: 'justify'}}>
+            Diverse terrain includes icy plains, intricate fractures, towering volcanoes, and hidden subsurface oceans. During its Goldilocks zone, lush landscapes with stunning beaches and bodies of water make it the ultimate beach vacation destination, complementing its cosmic wonders.
+            </p>
+          </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item style={{ height: '900px' }}>
+          <img src={require('./img/slide5.png')} style={{height: '700px', paddingTop: '180px'}} />
+
+          <Carousel.Caption>
+                        <h3 style={{fontSize: '35px'  }}>Satellite Communication</h3>
+            <p style={{textAlign: 'justify'}}>
+            To connect with the broader interstellar network and maintain communication with Earth and other spacefaring civilizations, the planet relies on a network of communication satellites in geostationary orbit around the planet. These satellites facilitate long-distance data transmission and interstellar connectivity.
+            </p>
+          </Carousel.Caption>
+          </Carousel.Item>
       </Carousel>
     </div>
   );
